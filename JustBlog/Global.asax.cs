78a321211c268cf38bec76fddd207e71ea8a3940
HriziMohamed <hrizienit@gmail.com>
2020-01-02 13:50:45 +0100

    Story 1: Fin Test

--- JustBlog/Global.asax.cs
+++ JustBlog/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using JustBlog.Core;
+using JustBlog.Providers;
+using System;
+
 namespace JustBlog
 {
     //  public class MvcApplication : System.Web.HttpApplication  
@@ -17,6 +20,8 @@ namespace JustBlog
             kernel.Load(new RepositoryModule());
             kernel.Bind<IBlogRepository>().To<BlogRepository>();
 
+            kernel.Bind<IAuthProvider>().To<AuthProvider>();
+
             return kernel;
         }
         protected override void OnApplicationStarted()
@@ -25,12 +30,39 @@ namespace JustBlog
             base.OnApplicationStarted();
         }
 
+
         //protected void Application_Start()
         //{
         //    AreaRegistration.RegisterAllAreas();
         //    FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         //    RouteConfig.RegisterRoutes(RouteTable.Routes);
         //    BundleConfig.RegisterBundles(BundleTable.Bundles);
+        //}
+
+
+        //public static void fizzBuzz(int n)
+        //{
+
+        //    for (int i = 1; i <= n; i++)
+        //    {
+        //        if (i % 5 == 0 && i % 3 == 0)
+        //        {
+        //            Console.WriteLine("FizzBuzz");
+        //            continue;
+        //        }
+        //        if (i % 5 == 0)
+        //        {
+        //            Console.WriteLine("Buzz");
+        //            continue;
+        //        }
+        //        if (i % 3 == 0)
+        //        {
+        //            Console.WriteLine("Fizz");
+        //            continue;
+        //        }
+
+        //        Console.WriteLine(i);
+        //    }
         //}
     }
 }
---
+++ JustBlog/Providers/IAuthProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace JustBlog.Providers
+{
+    public interface IAuthProvider
+    {
+        bool IsLoggedIn { get; }
+        bool Login(string username, string password);
+        void Logout();
+    }
+}
